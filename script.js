ecb0acc18031f46dab3b38a5e8a1c72dddf508e1
Your Name <you@example.com>
2024-12-02 10:34:49 +0000

    5ª tarefa cumprida

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", function (event) {
+
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+
+        form.classList.add("was-validated");
+    });
+});
